c5f7e51e3259cd71d7557990e97ae7ab79172316
Cuttlefish-chestnut <3395126@qq.com>
2019-08-08 17:54:04 +0800

    zhenzeng

--- js/008-抽取样式.js
+++ js/008-抽取样式.js
@@ -1,7 +1,6 @@
 function toTomato(){
 	document.getElementById('blue-box').style.backgroundColor="tomato"
 }
-// var blueBox = document.getElementById('blue-box')
 // blueBox.onclick=toTomato
 var blueBox = document.getElementById('blue-box')
 blueBox.onclick=function(){
